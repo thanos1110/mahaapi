bed61b1a5d54b28df6dfe05ee3976c6f9f63ba47
thanos1110 <86567121+thanos1110@users.noreply.github.com>
2026-01-11 15:39:36 +0530

    Modify getStore call to use environment variables
    Updated getStore to include siteID and token from environment variables.

--- netlify/functions/get.js
+++ netlify/functions/get.js
@@ -1,7 +1,11 @@
 import { getStore } from "@netlify/blobs";
 
 export async function handler() {
-  const store = getStore("licenses");
+//  const store = getStore("licenses");
+const store = getStore("licenses", {
+  siteID: process.env.NETLIFY_SITE_ID,
+  token: process.env.NETLIFY_AUTH_TOKEN
+});
 
   const { blobs } = await store.list();
   const result = [];
